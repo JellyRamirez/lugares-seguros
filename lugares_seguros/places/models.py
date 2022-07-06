2ab3d4c6f867a6bc80059f2eb0d4403e892b482a
JellyRamirez <ada_hack266ackademy.mx>
2022-07-06 18:50:33 -0500

    Places app created
    Place Model and Serializer

--- lugares_seguros/places/models.py
+++ lugares_seguros/places/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Place(models.Model):
+
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=256)
+    address_state = models.CharField(max_length=32)
+    address_city = models.CharField(max_length=32)
+    address_suburb = models.CharField(max_length=32)
+    address_street = models.CharField(max_length=32)
+    address_zipcode = models.CharField(max_length=32)
+
+    class Meta:
+        db_table = 'places'
+
+    def __str__(self) :
+        return self.name
