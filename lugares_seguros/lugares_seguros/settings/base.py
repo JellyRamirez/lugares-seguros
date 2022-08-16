8bd689ceee9415817d2358fc6463fe60a29e5fb5
Jele Ramírez <ada_hack266@hackademy.mx>
2022-08-16 01:15:23 -0500

    Commit

--- lugares_seguros/lugares_seguros/settings/base.py
+++ lugares_seguros/lugares_seguros/settings/base.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vjeab!6-0e_gn%b1!cd8nlgkz@!)3k7b@+4-dwxyu7(m7v&d76'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['lugares-seguros-jele.herokuapp.com']
 
 
 # Application definition
